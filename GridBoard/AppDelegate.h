40b0245dbb9acc85b1f0e80b48c9382f5cbd3906
Zach Fleischman <zfleisch@mines.edu>
2013-04-01 10:43:25 -0600

    Initial Commit

---
+++ GridBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GridBoard
+//
+//  Created by Zachary Fleischman on 4/1/13.
+//  Copyright (c) 2013 Zachary Fleischman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
